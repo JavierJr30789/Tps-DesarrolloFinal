93b12ee57fae7f6d5bbf7892c8ce8d96f421aa10
jere777xd <164878442+jere777xd@users.noreply.github.com>
2026-06-22 23:30:08 -0300

    modulos recursivos y primer duelo con arma magica

--- Cronograma.java
+++ Cronograma.java
@@ -5,10 +5,17 @@ public class Cronograma {
     private static final int cantDias = 7;
     private static final int cantHorarios = 15;
 
+    // constructor
     public Cronograma() {
         this.cronograma = new Duelo[cantDias][cantHorarios];
     }
 
+    // observador
+    public Duelo getDuelo(int dia, int hora) {
+        return cronograma[dia][horaIndice(hora)];
+    }
+
+    // propios del tipo
     public static int horaIndice(int hora) {
         return hora - 8;
     }
@@ -78,10 +85,6 @@ public class Cronograma {
         return agregado;
     }
 
-    public Duelo getDuelo(int dia, int hora) {
-        return cronograma[dia][horaIndice(hora)];
-    }
-
     public Duelo[] obtenerDuelosDelDia(int dia) {
         Duelo[] duelos = new Duelo[cantidadDuelosPorDia(dia)];
         int indiceDuelos = 0;
@@ -106,5 +109,66 @@ public class Cronograma {
         }
         return cantidadDuelos;
     }
-    //final del tda
+
+    public void recorrerMatriz(int indiceFila, int indiceCol) {
+        if (indiceFila <= cronograma.length) {
+
+        }
+    }
+
+    public int duelosRealizados(int indiceFila, int indiceCol, int cantDuelos) { // los 3 parametros deben iniciar en 0
+
+        if (indiceFila > cronograma.length - 1) {
+        } else if (indiceCol > cronograma[0].length - 1) {
+            cantDuelos = duelosRealizados(indiceFila + 1, 0, cantDuelos);
+        } else {
+            if (cronograma[indiceFila][indiceCol] != null &&
+                    cronograma[indiceFila][indiceCol].getEstado().equalsIgnoreCase("realizado")) {
+                cantDuelos++;
+            }
+            cantDuelos = duelosRealizados(indiceFila, indiceCol + 1, cantDuelos);
+        }
+
+        return cantDuelos;
+    }
+
+    public int cantidadHorariosLibres(int indiceFila, int indiceCol, int cantHorarios) { // los 3 parametros deben iniciar en 0
+
+        if (indiceFila > cronograma.length - 1) {
+        } else if (indiceCol > cronograma[0].length - 1) {
+            cantHorarios = cantidadHorariosLibres(indiceFila + 1, 0, cantHorarios);
+        } else {
+            if (cronograma[indiceFila][indiceCol] == null) {
+                cantHorarios++;
+            }
+            cantHorarios = cantidadHorariosLibres(indiceFila, indiceCol + 1, cantHorarios);
+        }
+
+        return cantHorarios;
+    }
+
+    public int[] primerDueloConArmaMagica() {
+        int j = 0;
+        boolean armaEncontrada = false;
+        int[] horarios = new int[7];
+
+        for (int i = 0; i < cronograma.length; i++) {
+            while (j < cronograma[0].length && !armaEncontrada) {
+                if (cronograma[i][j] != null && cronograma[i][j].usaArmaMagica()) {
+                    armaEncontrada = true;
+                    horarios[i] = cronograma[i][j].getHora();
+                }
+                j++;
+            }
+            if (!armaEncontrada) {
+                horarios[i] = -1;
+            }
+            j = 0;
+            armaEncontrada = false;
+        }
+
+        return horarios;
+    }
+
+    // final del tda
 }
